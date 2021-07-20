16ab288635a6d6fffdb261c8d633ac707c8270b0
volt64bolt <81176807+volt64bolt@users.noreply.github.com>
2021-07-20 15:12:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,27 @@
 
 
 //load discord.js library
-require('dotenv').config();
-const Discord = require('discord.js');
+require("dotenv").config();
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 //connect bot via token
-const token = 'ODY3MDIzMjg4ODQ4NDgyMzM1.YPbEXA.slwb8dkucpR315k-4Ld89Rp8_nc';
+const token = "ODY3MDIzMjg4ODQ4NDgyMzM1.YPbEXA.slwb8dkucpR315k-4Ld89Rp8_nc";
 
 //set the prefix
-const prefix = 'p!';
+const prefix = "p!";
 
 
 //execute on bot startup
-bot.on('ready', () => {
+bot.on("ready", () => {
 	//log when bot has started
-	console.log('The Potato Bot is up!');
+	console.log("The Potato Bot is up!");
 	//set custom status and idle
-	bot.user.setPresence({ activity: { name: 'p!help' }, status: 'idle' })
+	bot.user.setPresence({ activity: { name: "p!help" }, status: "idle" })
 });
 
 //main bulk of code
-bot.on('message', message =>{
+bot.on("message", message =>{
     
 	if(!message.content.startsWith(prefix) || message.author.bot) return;
 
@@ -32,19 +32,19 @@ bot.on('message', message =>{
     const command = args.shift().toLowerCase();
 
 	//test command
-    if(command === 'potato'){
-        message.channel.send('POTATO');
+    if(command === "potato"){
+        message.channel.send("POTATO");
     }
 	//help menu
-	if(command === 'help'){
+	if(command === "help"){
 		message.channel.send(":potato: this is help menu. \n p!help \n p!comp \n p!potato \n p!stats");
       }
 	 
 	 //random comp
-	if(command === 'comp'){
-		var methods = ["drawing, 3D modelling, sketching, painting, pixel-arting, making minecraft thingy of"];
+	if(command === "comp"){
+		var methods = ["drawing", "3D modelling", "sketching", "painting", "pixel-arting", "making a minecraft build of"];
 		var method = Math.floor(Math.random() * methods.length);
-		var themes = ["a cat, cats, a potato, potatos, a dog, dogs, a fish, fish, a dragon, dragons, a wild jel, mars, stinky cheese, an angler fish, angler fish, a whale, whales"];
+		var themes = ["a cat", "cats", "a potato", "potatos", "a dog", "dogs", "a fish", "fish", "a dragon", "dragons", "a wild jel", "mars", "stinky cheese", "an angler fish", "angler fish", "a whale", "whales"];
 		var theme = Math.floor(Math.random() * themes.length);
 		var actions = ["dying, "];
 		var action = Math.floor(Math.random() * actions.length);
@@ -52,20 +52,20 @@ bot.on('message', message =>{
 	}
 
 	//p!potato
-	if(command === 'potato'){
-	message.react('🥔');
-	message.channel.send('There ya go :D :potato: ');
+	if(command === "potato"){
+	message.react("🥔");
+	message.channel.send("Here :potato: ");
 	
 	//vars for randomisation
-	var potatos = ["https://freshpointlocal.co.uk/wp-content/uploads/2018/12/Potatoes-2.jpg, https://cdn-a.william-reed.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/nutraingredients.com/article/2020/05/07/study-potato-protein-a-winner-for-women/11369572-1-eng-GB/Study-Potato-protein-a-winner-for-women_wrbm_large.jpg, https://api.time.com/wp-content/uploads/2020/04/Boss-Turns-Into-Potato.jpg, https://bellavitashop.co.uk/6180-large_default/potatoes-500g.jpg"];
+	var potatos = ["https://freshpointlocal.co.uk/wp-content/uploads/2018/12/Potatoes-2.jpg", "https://cdn-a.william-reed.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/nutraingredients.com/article/2020/05/07/study-potato-protein-a-winner-for-women/11369572-1-eng-GB/Study-Potato-protein-a-winner-for-women_wrbm_large.jpg", "https://api.time.com/wp-content/uploads/2020/04/Boss-Turns-Into-Potato.jpg, https://bellavitashop.co.uk/6180-large_default/potatoes-500g.jpg"];
 	var potato = Math.floor(Math.random() * potatos.length);
 	
 	//set const for embed (image)
 	const potatoEmbed = new Discord.MessageEmbed()
 	
 	//set properties of embed (image)
-	.setColor('#FFBD33')
-	.setTitle('Here is your cheese!')
+	.setColor("#FFBD33")
+	.setTitle("Here is your cheese!")
 	.setImage(potatos[potato])
 
 	
@@ -74,8 +74,14 @@ bot.on('message', message =>{
 	}
 
 	//P!stats command
-	if (command === 'stats')
+	if (command === "stats")
 	message.channel.send(`serving **${bot.guilds.cache.size}** servers`)
+
+	//p!killniesbot
+	if (command === "killniesbot")
+	var roasts = [""]
+	var roast = Math.floor(Math.random() * roasts.length)
+	message.channel.send(roasts[roast])
 
 //end of main code
 });
